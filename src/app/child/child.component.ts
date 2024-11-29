6a6666243b63c28d614e1c8523ff7d07f30e18b5
Ben Salem Meryem <124097610+MeryemBenSalem@users.noreply.github.com>
2024-11-29 13:39:51 +0100

    Update child.component.ts

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -7,10 +7,11 @@ import { Component, Output, EventEmitter,Input } from '@angular/core';
 })
 export class ChildComponent {
   myFavoriteColor: string = 'lightgreen';  
-  @Output() colorChanged: EventEmitter<string> = new EventEmitter();  
-
-  @Input() color: string = '';
+  @Output() colorChanged: EventEmitter<string> = new EventEmitter(); 
   changeColor(): void {
     this.colorChanged.emit(this.myFavoriteColor);  // Envoi de la couleur au parent
-  }
+  } 
+
+  @Input() color: string = '';
+  
 }
